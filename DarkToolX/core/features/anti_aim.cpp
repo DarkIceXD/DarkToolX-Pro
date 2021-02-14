f6e6195b7991e0602bc78411ae0d06e55f831ff4
DarkIceXD <15961071+DarkIceXD@users.noreply.github.com>
2021-02-14 23:50:11 +0100

    Update anti_aim.cpp

--- DarkToolX/core/features/anti_aim.cpp
+++ DarkToolX/core/features/anti_aim.cpp
@@ -1,16 +1,40 @@
 #include "features.hpp"
 
-void apply_desync(c_usercmd* cmd, bool& send_packet, const bool desync_left)
+static bool updating_lby()
 {
+	static auto lby_time = 0.f;
+	const auto anim_state = csgo::local_player->get_anim_state();
+	if (!anim_state)
+		return false;
+
+	const auto curtime = csgo::local_player->get_tick_base() * interfaces::globals->interval_per_tick;
+	if (anim_state->velocity > 0.1f || std::abs(anim_state->jump_fall_velocity) > 100.0f)
+	{
+		lby_time = curtime + 0.22f;
+	}
+	else if (curtime > lby_time)
+	{
+		lby_time = curtime + 1.1f;
+		return true;
+	}
+	return false;
+}
+
+static void apply_desync(c_usercmd* cmd, bool& send_packet)
+{
+	const auto desync_left = cmd->sidemove < 0;
 	const auto max_desync = std::abs(csgo::local_player->max_desync_angle());
-	if (send_packet)
+	if (updating_lby())
+	{
+		cmd->viewangles.y += desync_left ? -120 : 120;
+		send_packet = false;
+	}
+	else if (!send_packet)
 	{
 		cmd->viewangles.y += desync_left ? max_desync : -max_desync;
 	}
-	else
-	{
-		cmd->viewangles.y += desync_left ? -120 : 120;
-	}
+	/*if (!send_packet)
+		cmd->viewangles.y += desync_left ? max_desync : -max_desync;*/
 }
 
 void features::anti_aim(c_usercmd* cmd, bool& send_packet)
@@ -52,7 +76,6 @@ void features::anti_aim(c_usercmd* cmd, bool& send_packet)
 	if (csgo::conf->misc().smart_anti_aim && csgo::target.entity)
 		cmd->viewangles = csgo::target.angle;
 
-	constexpr auto desync_left = true;
 	switch (csgo::conf->misc().anti_aim)
 	{
 	default:
@@ -60,12 +83,12 @@ void features::anti_aim(c_usercmd* cmd, bool& send_packet)
 		cmd->viewangles.y += 180;
 		break;
 	case 2:
-		apply_desync(cmd, send_packet, desync_left);
+		apply_desync(cmd, send_packet);
 		break;
 	case 3:
 		cmd->viewangles.x = 89;
 		cmd->viewangles.y += 180;
-		apply_desync(cmd, send_packet, desync_left);
+		apply_desync(cmd, send_packet);
 		break;
 	}
 }
