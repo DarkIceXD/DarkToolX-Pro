226cc141da50d7141efffa12e7bdd5c2c7c7515e
DarkIceXD <15961071+DarkIceXD@users.noreply.github.com>
2021-02-17 15:06:52 +0100

    Update hooks.cpp

--- DarkToolX/core/hooks/hooks.cpp
+++ DarkToolX/core/hooks/hooks.cpp
@@ -299,7 +299,7 @@ void __stdcall hooks::emit_sound::hook(void* filter, int iEntIndex, int iChannel
 
 long __stdcall hooks::end_scene::hook(IDirect3DDevice9* device)
 {
-	static auto water_mark = std::string("DarkToolX Pro - beta v6.4 - UID: ") + std::to_string(csgo::user.uid);
+	static auto water_mark = std::string("DarkToolX Pro - beta v6.5 - UID: ") + std::to_string(csgo::user.uid);
 	IDirect3DStateBlock9* pixel_state = NULL;
 	device->CreateStateBlock(D3DSBT_ALL, &pixel_state);
 	pixel_state->Capture();
