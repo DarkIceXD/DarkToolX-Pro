51a88f95ae2246684d5426ee330062fa2cfd2c4e
DarkIceXD <15961071+DarkIceXD@users.noreply.github.com>
2021-07-10 17:29:13 +0200

    fixed glow

--- DarkToolX/dependencies/interfaces/glow_manager.hpp
+++ DarkToolX/dependencies/interfaces/glow_manager.hpp
@@ -8,23 +8,25 @@ public:
 		alpha = a;
 		render_when_occluded = true;
 		render_when_unoccluded = false;
-		bloom_amount = 1.0f;
 	}
 	bool unused() {
 		return next_free_slot != -2;
 	}
 
-	void* entity;
+	int next_free_slot;
+	player_t* entity;
 	vec3_t color;
 	float alpha;
-	char unknown0[8];
-	float bloom_amount;
-	char unknown1[4];
+	bool m_bGlowAlphaCappedByRenderAlpha;
+	float m_flGlowAlphaFunctionOfMaxVelocity;
+	float m_flGlowAlphaMax;
+	float m_flGlowPulseOverdrive;
 	bool render_when_occluded;
 	bool render_when_unoccluded;
 	bool full_bloom_render;
-	char unknown2[13];
-	int next_free_slot;
+	int m_nFullBloomStencilTestValue;
+	int render_style;
+	int m_nSplitScreenSlot;
 };
 
 class glow_manager_t {
